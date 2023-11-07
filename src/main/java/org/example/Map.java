3a50a66931f1c3012ab16b6cd017db8b7678df77
BerzanDat23c <berzoo2904@gmail.com>
2023-11-07 08:52:37 +0100

    Commit Update:

--- src/main/java/org/example/Map.java
+++ src/main/java/org/example/Map.java
@@ -1,29 +1,47 @@
 package org.example;
 
+//Metode
 public class Map {
+
+    //Attributter
     private Room currentRoom;
 
+    //Kontruktør (ingen argumenter i parenteserne, hvilket betyder, at den er en "default constructor)
     public Map() {
         initializeRooms();
     }
 
+
+
+    //fortæl om rummene
     private void initializeRooms() {
+
         // Opret de forskellige rum
         Room room1 = new Room("Room 1", "A dark room with two doors to the south and east.");
-
         room1.addItem(new MeleeWeapon("sword","does physical damage",15)); // navn, skade
 
         Room room2 = new Room("Room 2", "A dark room with two doors to the west and east.");
+
         Room room3 = new Room("Room 3", "A dark room with two doors to the west and south.");
+
         Room room4 = new Room("Room 4", "A dark room with two doors to the north and south.");
+
         Room room5 = new Room("Room 5", "A dark room with a door to the south.");
         room5.addItem(new RangedWeapon("pistol","shoot bullets",20,7)); // navn, skade, skud
+
         Room room6 = new Room("Room 6", "A dark room with two doors to the north and south.");
+
         Room room7 = new Room("Room 7", "A dark room with two doors to the north and east.");
         room7.addItem(new RangedWeapon("bow","shoot arrows",10,10));
-        Room room8 = new Room("Room 8", "A dark room with three doors to the west, north, and east.");
-        Room room9 = new Room("Room 9", "A dark room with two doors to the north and west.");
 
+        Room room8 = new Room("Room 8", "A dark room with three doors to the west, north, and east.");
+
+        Room room9 = new Room("Room 9", "A dark room with two doors to the north and west.");
+        //RangedWeapons er våben, der er designet til at angribe fjender på afstand.
+        //MeleeWeapons er våben, der bruges til nærtkamp.
+
+
+        //fortæl om det her
         Food apple = new Food("Apple", "A red apple", 10);
         room1.addItem(apple);
 
